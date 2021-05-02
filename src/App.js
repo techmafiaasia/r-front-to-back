49a2f282d21561107a633271e838c0ad37bb3028
Ayush Bajracharya <bajracharya.ayush@webexpertsnepal.com>
2021-05-02 15:38:50 +0545

    boostrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Contact from './components/Contact'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default function App() {
   return (
     <div>
-      <Contact />
+      <Header branding="Contact Manager 1" />
+      <div className="container">
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-55-5555" />
+      </div>
     </div>
   )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Contact() {
+export default function Contact(props) {
     return (
-        <div>
-            Contact
+        <div className="card card-body mb-3">
+            <h4>{props.name}</h4>
+            <ul className="list-group">
+                <li className="list-group-item">{props.email}</li>
+                <li className="list-group-item">{props.phone}</li>
+            </ul>
         </div>
     )
+}
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
 }
